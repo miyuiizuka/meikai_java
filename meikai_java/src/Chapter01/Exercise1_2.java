3fd976668816eaa3b4378d05dfb23bc396e2132e
miyuiizuka <ddt.my.iizuka@gmail.com>
2024-10-01 15:22:01 +0900

    演習1-2が終わりましたので、ご確認お願いいたします。

---
+++ meikai_java/src/Chapter01/Exercise1_2.java
@@ -0,0 +1,21 @@
+package Chapter01;
+/*
+ * クラス名:Exercise1_2
+ * 	概要:各行に1文字ずつ名前を表示するプログラム
+ * 作成者:M.Iizuka
+ * 作成日:2024/10/01
+ */
+public class Exercise1_2 {
+	
+	/*関数名:main
+	 * 概要:各行に1文字ずつ名前を表示するプログラム
+	 * 引数:なし
+	 * 戻り値:なし
+	 * 作成者:M.Iizuka
+	 * 作成日:2024/10/01
+	 */
+	public static void main(String[] args) {
+		//各行に1文字ずつ名前を表示する
+		System.out.println("飯\n塚\n美\n友");
+	}
+}
