40fe223f0eb91578b523dc4aeab96fe60a88a0d7
miyuiizuka <ddt.my.iizuka@gmail.com>
2024-10-01 13:44:28 +0900

    テストコミット

---
+++ meikai_java/src/Chapter01/TestClass.java
@@ -0,0 +1,14 @@
+package Chapter01;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		int a = 0;
+		int b = 2;
+		int c = a+b;
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello World");
+
+	}
+
+}
