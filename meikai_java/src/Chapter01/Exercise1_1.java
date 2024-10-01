11c1392acf28482acd6e5aafa5201a5aeb82969c
miyuiizuka <ddt.my.iizuka@gmail.com>
2024-10-01 14:21:52 +0900

    演習1―1が終わりましたので、ご確認お願いいたします。

---
+++ meikai_java/src/Chapter01/Exercise1_1.java
@@ -0,0 +1,23 @@
+package Chapter01;
+
+/*
+ * クラス名:Exercise1_1
+ * 	概要:セミコロンが欠如している場合のテスト
+ * 作成者:M.Iizuka
+ * 作成日:2024/10/01
+ */
+public class Exercise1_1 {
+
+	/*関数名:main
+	 * 概要:セミコロンが欠如している場合のテスト
+	 * 引数:なし
+	 * 戻り値:なし
+	 * 作成者:M.Iizuka
+	 * 作成日:2024/10/01
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("初めてのJavaプログラム。");
+		System.out.println("画面に出力しています。")
+	}
+}
