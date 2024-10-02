2333562c7cb8893856aef51f01721ee4e546deaa
miyuiizuka <ddt.my.iizuka@gmail.com>
2024-10-02 14:15:06 +0900

    演習2-7を修正しましたので、ご確認お願いいたします。

--- meikai_java/src/Chapter02/Exercise2_7.java
+++ meikai_java/src/Chapter02/Exercise2_7.java
@@ -24,14 +24,25 @@ public class Exercise2_7 {
 		//乱数を抽出するための変数を宣言する
 		Random randomNumber = new Random();
 		
+		//乱数の範囲を指定する変数を宣言し、0から8に設定する
+		int rangeInt = 9;
+		//乱数の範囲を調節する変数を宣言し、1から9の範囲に調整するための値を代入する
+		int ajustInt = 1;
+		
 		//抽出した乱数を読み込み、変数に1から9のいずれかを格納する
-		int singleDigitNumber = randomNumber.nextInt(9) + 1;
+		int singleDigitNumber = randomNumber.nextInt(rangeInt) + ajustInt;
 		
+		//-9から-1の範囲に調節するための値を代入する
+		ajustInt = 9;
 		//抽出した乱数を読み込み、変数に-9から-1のいずれかを格納する
-		int negativeSingleDigitNumber = randomNumber.nextInt(9) - 9;
+		int negativeSingleDigitNumber = randomNumber.nextInt(rangeInt) - ajustInt;
 		
+		//乱数の範囲を0から89に指定する
+		rangeInt = 90;
+		//10から99の範囲に調節するための値を代入する
+		ajustInt= 10;		
 		//抽出した乱数を読み込み、変数に10から99のいずれかを格納する
-		int twoDigitNumber = randomNumber.nextInt(90) + 10;
+		int twoDigitNumber = randomNumber.nextInt(rangeInt) + ajustInt;
 		
 		//1桁の正の整数値を表示する
 		System.out.println( singleDigitNumber );
