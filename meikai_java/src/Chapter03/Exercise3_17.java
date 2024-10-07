53804aa7eabf791783a5c1366cd69c3a7cc1921a
miyuiizuka <ddt.my.iizuka@gmail.com>
2024-10-07 16:56:57 +0900

    3-17を再度修正したのでご確認お願いいたします。

--- meikai_java/src/Chapter03/Exercise3_17.java
+++ meikai_java/src/Chapter03/Exercise3_17.java
@@ -28,25 +28,32 @@ public class Exercise3_17 {
 		//抽出された乱数を変数に格納する
 		int handRandom = randomNumber.nextInt(SUM_HAND);
 
+		//グーの番号
+		final int ROCK_NUMBER = 0;
+		//チョキの番号
+		final int SCISSORS_NUMBER = 1;
+		//パーの番号
+		final int PAPER_NUMBER = 2;
+
 		//手の種類
 		String handName = "";
 
 		//乱数による手の種類
 		switch (handRandom) {
 		//グーの番号が生成された場合
-		case SUM_HAND-3:
+		case ROCK_NUMBER:
 			//グーを格納
 			handName = "グー";
 			//switch文を抜ける
 			break;
 		//チョキの番号が生成された場合
-		case SUM_HAND-2:
+		case SCISSORS_NUMBER:
 			//チョキを格納
 			handName = "チョキ";
 			//switch文を抜ける
 			break;
 		//パーの番号が生成された場合
-		case SUM_HAND-1:
+		case PAPER_NUMBER:
 			//パーを格納
 			handName = "パー";
 			//switch文を抜ける
